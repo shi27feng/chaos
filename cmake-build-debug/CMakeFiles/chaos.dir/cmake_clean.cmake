b96654df2d3ba8254db7b4ad393605ebb62a7570
shi27feng <shi.feng@cs.ucla.edu>
2017-11-21 20:45:06 -0800

    cmake folder added

---
+++ cmake-build-debug/CMakeFiles/chaos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chaos.dir/test.cpp.o"
+  "chaos.pdb"
+  "chaos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chaos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
